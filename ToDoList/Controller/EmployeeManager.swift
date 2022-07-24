97459a1b59b7577c537f47285f49ec01794ccb72
Nikita <Tomskrenta@mail.ru>
2022-07-25 10:38:10 +1200

    employees

--- ToDoList/Controller/EmployeeManager.swift
+++ ToDoList/Controller/EmployeeManager.swift
@@ -1,9 +1,44 @@
 import Foundation
 
 class EmployeeManager: NSObject {
-    let employees: [Employee]
+    var employees: [[Employee]]
+    var accountants: [Accountant]
+    let managers: [Management]
+    var basicWorkers: [BasicWorker]
 
     override init() {
-        employees = [Employee(name: "Vasia", salary: 40.00)]
+        accountants = [
+            Accountant(
+                name: "Valera",
+                salary: 500.00,
+                breakHours: TimePeriod(startTime: Date(timeIntervalSince1970: 20), finishTime: Date(timeIntervalSince1970: 30)),
+                deskNumber: 5,
+                accountantType: .payroll
+            )
+        ]
+
+        managers = [
+            Management(
+                name: "Manager444",
+                salary: 400.00,
+                meetingHours: TimePeriod(
+                    startTime: Date(timeIntervalSince1970: 40),
+                    finishTime: Date(timeIntervalSince1970: 50)
+                )
+            )
+        ]
+
+        basicWorkers = [
+            BasicWorker(
+                name: "Nikoglay",
+                salary: 20.00,
+                breakHours: TimePeriod(
+                    startTime: Date(timeIntervalSince1970: 23),
+                    finishTime: Date(timeIntervalSince1970: 54)
+                ),
+                deskNumber: 5
+            )
+        ]
+        employees = [managers, accountants, basicWorkers]
     }
 }
