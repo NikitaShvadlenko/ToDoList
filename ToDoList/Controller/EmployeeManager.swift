b5fb1054190f27120f60daedfbb37eb8ed8fa401
Nikita <Tomskrenta@mail.ru>
2022-08-01 18:09:33 +1200

    Append employees
    Append employees

--- ToDoList/Controller/EmployeeManager.swift
+++ ToDoList/Controller/EmployeeManager.swift
@@ -4,18 +4,21 @@ protocol EmployeeManagerProtocol {
     func fetchEmployeeData(from provider: EmployeeProvider)
     func addEmployee(employee: Employee)
     func removeEmployee(employee: Employee)
+    func updateEmployeeList(with employee: Employee)
 }
 
 class EmployeeManager: NSObject {
+    let employeeDataProvider = EmployeeProvider()
+
     var employees: [[Employee]] = []
-//    var accountants: [Accountant] = []
-//    var managers: [Manager] = []
-//    var basicWorker: [BasicWorker] = []
+
+    private var accountants: [Accountant] = []
+    private var managers: [Manager] = []
+    private var basicWorkers: [BasicWorker] = []
 
     override init() {
-        let employeeDataProvider = EmployeeProvider()
-        employees = employeeDataProvider.fetchEmployeeData()
         super.init()
+        fetchEmployeeData(from: employeeDataProvider)
         addEmployee(employee: Accountant(name: "cwdc", salary: 100500, breakHours: TimePeriod(startTime: Date(), finishTime: Date()), deskNumber: 12, accountantType: .inventory))
         addEmployee(employee: BasicWorker(name: "de", salary: 300, breakHours: TimePeriod(startTime: Date(), finishTime: Date()), deskNumber: 11))
         addEmployee(employee: Manager(name: "dwd", salary: 3000, meetingHours: TimePeriod(startTime: Date(), finishTime: Date())))
@@ -25,8 +28,7 @@ class EmployeeManager: NSObject {
 // MARK: EmployeeManagerProtocol
 extension EmployeeManager: EmployeeManagerProtocol {
     func addEmployee(employee: Employee) {
-        // switch employee type, add to relevant group, renew the array.
-        employees[2].append(employee)
+        updateEmployeeList(with: employee)
     }
 
     func removeEmployee(employee: Employee) {
@@ -34,5 +36,31 @@ extension EmployeeManager: EmployeeManagerProtocol {
 
     func fetchEmployeeData(from provider: EmployeeProvider) {
         self.employees = provider.fetchEmployeeData()
+
+        for employeeType in employees {
+            for employee in employeeType {
+                updateEmployeeList(with: employee)
+            }
+        }
+    }
+
+    func updateEmployeeList(with employee: Employee) {
+
+        switch employee.employeeType {
+
+        case .management:
+            guard let manager = employee as? Manager else { return }
+            managers.append(manager)
+
+        case .accountant:
+            guard let accountant = employee as? Accountant else { return }
+            accountants.append(accountant)
+
+        case .basicWorker:
+            guard let basicWorker = employee as? BasicWorker else { return }
+            basicWorkers.append(basicWorker)
+        }
+
+        employees = [managers, basicWorkers, accountants]
     }
 }
